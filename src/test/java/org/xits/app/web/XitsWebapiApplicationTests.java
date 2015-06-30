a7a9421ff1ec9d75f086424ad27c48e074e1dfe6
angora <pacouley@gmail.com>
2015-06-30 06:26:23 +0200

    first commit

---
+++ src/test/java/org/xits/app/web/XitsWebapiApplicationTests.java
@@ -0,0 +1,18 @@
+package org.xits.app.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = XitsWebapiApplication.class)
+@WebAppConfiguration
+public class XitsWebapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
